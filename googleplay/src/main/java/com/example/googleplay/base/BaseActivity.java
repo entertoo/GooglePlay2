3518d66086ec8da677b8a17d5256c17425251c95
Haoping <56639624@qq.com>
2017-04-06 18:10:39 +0800

    little change for runtime permissions

--- googleplay/src/main/java/com/example/googleplay/base/BaseActivity.java
+++ googleplay/src/main/java/com/example/googleplay/base/BaseActivity.java
@@ -62,14 +62,15 @@ public abstract class BaseActivity extends AppCompatActivity {
         // if not get permission
         if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
             // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)) {
-
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                System.out.println("shouldShowRequestPermissionRationale: true");
                 // Show an expanation to the user *asynchronously* -- don't block
                 // this thread waiting for the user's response! After the user
                 // sees the explanation, try again to request the permission.
+                Toast.makeText(this, "Permission not get", Toast.LENGTH_SHORT).show();
 
             } else {
-
+                System.out.println("shouldShowRequestPermissionRationale: false");
                 // No explanation needed, we can request the permission.
 
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
@@ -96,9 +97,10 @@ public abstract class BaseActivity extends AppCompatActivity {
 
                     // permission was granted, yay! Do the
                     // contacts-related task you need to do.
+                    System.out.println("onRequestPermissionsResult: true");
 
                 } else {
-
+                    System.out.println("onRequestPermissionsResult: false");
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
                 }
